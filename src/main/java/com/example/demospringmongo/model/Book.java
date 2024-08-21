1d5bdea8f38a43a689195a33d4fbb59f98475f84
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-08-21 14:34:23 +0700

    [UPDATE] Book model to implement Serializable interface

--- src/main/java/com/example/demospringmongo/model/Book.java
+++ src/main/java/com/example/demospringmongo/model/Book.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @Document(collection = "books")
-public class Book {
+public class Book implements Serializable {
     @Id
     private String id;
     private String title;
